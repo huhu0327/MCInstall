10b2bd76bf3091100d152b61fce7ad9096b92c95
Dareum <ndr.developer@gmail.com>
2020-10-07 17:17:03 +0900

    Replace prism project

---
+++ MCInstall/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace MCInstall.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "MCInstall";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
