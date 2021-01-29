b954cf3c0031d042136c8cec5117c00913a2eb83
Dareum Nam <ndr.developer@gmail.com>
2021-01-29 12:59:17 +0900

    Create ActionCommand

---
+++ src/MCInstall/Commands/ActionCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MCInstall.Commands
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> _action;
+        private readonly Predicate<Object> _predicate;
+
+        public ActionCommand(Action<Object> action) : this(action, null) { }
+
+        public ActionCommand(Action<Object> action, Predicate<Object> predicate)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action), @"You must specify an Action<T>.");
+            _predicate = predicate;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var result = _predicate?.Invoke(parameter);
+            return !result.HasValue || result.Value;
+        }
+
+        public void Execute() => Execute(null);
+
+        public void Execute(object parameter) => _action?.Invoke(parameter);
+    }
+}
