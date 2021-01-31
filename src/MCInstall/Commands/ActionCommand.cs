10f5fc25b8174ce42fa5fdb805e9e4d3d3f99447
Dareum Nam <ndr0271@HUHU_LAPTOP.localdomain>
2021-02-01 07:55:06 +0900

    fix canexcute bool value

--- src/MCInstall/Commands/ActionCommand.cs
+++ src/MCInstall/Commands/ActionCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace MCInstall.Commands
@@ -6,11 +7,9 @@ namespace MCInstall.Commands
     public class ActionCommand : ICommand
     {
         private readonly Action<object> _action;
-        private readonly Predicate<Object> _predicate;
+        private readonly Predicate<object> _predicate;
 
-        public ActionCommand(Action<Object> action) : this(action, null) { }
-
-        public ActionCommand(Action<Object> action, Predicate<Object> predicate) => (_action, _predicate) = (action ?? throw new ArgumentNullException(nameof(action), @"You must specify an Action<T>."), predicate);
+        public ActionCommand(Action<object> action, Predicate<object> predicate = null) => (_action, _predicate) = (action ?? throw new ArgumentNullException(nameof(action), @"You must specify an Action<T>."), predicate);
 
         public event EventHandler CanExecuteChanged
         {
@@ -18,13 +17,7 @@ namespace MCInstall.Commands
             remove => CommandManager.RequerySuggested -= value;
         }
 
-        public bool CanExecute(object parameter)
-        {
-            var result = _predicate?.Invoke(parameter);
-            return !result.HasValue || result.Value;
-        }
-
-        public void Execute() => Execute(null);
+        public bool CanExecute(object parameter) => _predicate?.Invoke(parameter) ?? true;
 
         public void Execute(object parameter) => _action?.Invoke(parameter);
     }
